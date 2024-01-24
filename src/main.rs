f329a71bb6ebed9b29d26b6af1f21672e58de888
ola-lekan <78583289+ola-lekan@users.noreply.github.com>
2024-01-24 22:12:08 +0100

    still trying to figure out y true cases aint working

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,20 @@
-use std::{fs::File, io::{BufRead, BufReader}};
-
-use walkdir::{DirEntry, WalkDir};
-
-
-// fn is_hidden(entry: &DirEntry) -> bool {
-//     entry.file_name()
-//          .to_str()
-//          .map(|s| s.contains(r"y2mate.com - "))
-//          .unwrap_or(false)
-// }
+use walkdir::WalkDir;
 
 fn main() {
-
-
-let walker = WalkDir::new(r"C:\Users\pc\Downloads\test").into_iter();
-    for entry in walker.filter_map(|e|e.ok()){
-            if entry.file_type().is_file(){
-                let file_path = entry.path();
-                if let Ok(file)= File::open(&file_path){
-                    let reader = BufReader::new(file);
-
-                    for (line_number, line) in reader.lines().enumerate() {
-                        if let Ok(line_content) = line{
-                            if line_content.contains(r"y2mate.com"){
-                                        println!("keyword found in {}: Line {} -{}",
-                                            file_path.display(),
-                                            line_number + 1,
-                                            line_content
-                        );
-                            }
-                        }
-                    }   
-                }else{
-                    eprintln!("Error opening file: {}", file_path.display());
-                }
-            }
+    let walker = WalkDir::new(r"C:\Users\pc\Downloads\test").into_iter();
+    for entry in walker
+        .filter_entry(|e| {
+            //println!("{:?}", e.file_name().to_str());
+            e.file_name()
+                .to_str()
+                .map(|s| {
+                    println!("{}", s.contains("y"));
+                    s.contains("y")
+                })
+                .unwrap_or(false)
+        })
+        .filter_map(|v| v.ok())
+    {
+        println!("{}", entry.path().display());
     }
 }
