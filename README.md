b8f5fa10357767b8e4c049b3c5eb7de5c35e0fb6
ola-lekan <78583289+ola-lekan@users.noreply.github.com>
2024-01-25 22:05:26 +0100

    added clap and updated the read me

--- README.md
+++ README.md
@@ -7,8 +7,26 @@ This cli program allows you to rename filename in bulk, it also deletes file wit
 So i download a lot of videos from youtube because you can only watch one video at a time obviously. and sometimes i download videos from youtube. the video name starts with `y2mate.com - a - ` which i don't like.
 
 ## quick start
+1. clone the repo
+`https://github.com/ahmad-olu/Duplicate-Explorer.git`
+2. run the command below replacing each parts with the using the usage and make sure your feature flag is replace to use the replace feature
+`cargo run -- -f replace -p C:\Users\pc\Downloads\test -c aCopy -r bCopy`
 ## usage
+
+```
+    -f, --feature <FEATURE>            the type of feature
+    -p, --path <PATH>                  the path to read
+    -c, --contains <CONTAINS>          the value to be replaces
+    -r, --replace-with <REPLACE_WITH>  What to replace it with
+    -h, --help                         Print help
+    -V, --version                      Print version
+```
 ## contribution
 --------------
 # This project is not completed yet 
+
+### other feature to come
+- duplicate file finder(both with hashing and just file name)
+- deleting file with a particular pattern
+- file explorer
 --------------
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,49 @@
+use clap::{arg, command, Parser};
 use std::fs;
 use winwalk::*;
 
+/// Simple program to greet a person
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// the type of feature
+    #[arg(short, long)]
+    feature: String,
+    /// the path to read
+    #[arg(short, long)]
+    path: String,
+
+    /// the value to be replaces
+    #[arg(short, long)]
+    contains: String,
+
+    /// What to replace it with
+    #[arg(short, long)]
+    replace_with: String,
+}
+
 fn main() {
-    for file in walkdir(r"C:\Users\pc\Downloads\test", 1)
-        .into_iter()
-        .flatten()
-    {
-        if file.name.starts_with("y2mate.com - a - ") {
+    //cargo run -- -f hello
+    let args = Args::parse();
+
+    if args.feature.to_lowercase() == "replace" {
+        find_and_rename(
+            args.path.as_str(),
+            args.contains.as_str(),
+            args.replace_with.as_str(),
+        )
+    }
+}
+
+fn find_and_rename(path: &str, contains: &str, replace_with: &str) {
+    //path = r"C:\Users\pc\Downloads\test"
+    //contains = "y2mate.com - a - "
+    //replace_with = "a"
+    for file in walkdir(path, 0).into_iter().flatten() {
+        if file.name.contains(contains) {
             //println!("{:#?}", file);
             //println!("{}", file.path);
-            let new_name_path = file.path.replace("y2mate.com - a - ", "a");
+            let new_name_path = file.path.replace(contains, replace_with);
             fs::rename(file.path, new_name_path).expect("error renaming file");
         }
     }
